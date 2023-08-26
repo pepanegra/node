b3b78ef35a95ef4bfaaf487380629cc8853e1fe5
Jonathan Angulo <jonathangabrielangulo95@gmail.com>
2023-08-26 08:49:01 -0500

    actualizacion

--- 02-modulos-nativos/4-fs-stat.js
+++ 02-modulos-nativos/4-fs-stat.js
@@ -1,3 +1,13 @@
-const fs = require("node:fs")
+// import fs from "node:fs";
+const fs = require("node:fs");
 
-const stat = fs.s
+const stat = fs.statSync("./archivo.txt");
+
+console.log("Es un directorio?");
+console.log(stat.isDirectory());
+
+console.log("Es un archivo?");
+console.log(stat.isFile());
+
+console.log("Tamano del archivo en bytes:");
+console.log(stat.size);
