5580ea3fb0de61536333f0a5188030ed55adac6d
Jonathan Angulo <jonathangabrielangulo95@gmail.com>
2023-08-29 16:12:36 -0500

    Actualizacion

--- 03-http/1.http.js
+++ 03-http/1.http.js
@@ -1,13 +1,24 @@
 const http = require("node:http")
+const fs = require("node:fs");
 
 const server = http.createServer((req,res) => {
     if ( req.url === "/") {
         res.setHeader("Content-type", "text/html; charset=utf-8")
-        res.end("Hello this is my frist web page!")
-    } else {
-        res.statusCode = 404
-        res.end("<p>not found</p>")
+        res.end("<h1>Hello this is my frist web page!</h1>")
+    }else if (res.url === "/imagen") {
+        res.end("<p>Hello i am jonathan</p>") 
+        fs.readFile("./imagen.png", (err, data) => {
+         if(err){
+            res.end("<h1>Internal error</h1>")
+            res.statusCode = 500
+            
+         }else{
+            res.setHeader("content_Type", "image/png")
+            res.end(data)
+         }
+        }) 
     }
+    
 })
 
 server.listen(3000, () => {
